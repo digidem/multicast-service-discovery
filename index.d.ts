688c14cbca1209ed1e2e373d78aa314f9f1eca01
sethvincent <sethvincent@gmail.com>
2022-06-24 18:26:30 -0700

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,22 @@
 export class MdnsDiscovery extends TypedEmitter<MdnsDiscoveryEvents> {
-  constructor();
-  lookup(name: string): Promise<void>;
-  stopLookup(): Promise<void>;
-  announce(
-    name: string,
-    options: {
-      port: number;
-      txt: object;
-    }
-  ): void;
-  updateTxt(txt: object): void;
-  unannounce(immediate?: boolean | undefined): void;
-  destroy(): void;
-  #private;
+    constructor();
+    lookup(name: string): Promise<void>;
+    stopLookup(): Promise<void>;
+    announce(name: string, options: {
+        port: number;
+        txt: object;
+    }): void;
+    updateTxt(txt: object): void;
+    unannounce(immediate?: boolean | undefined): void;
+    destroy(): void;
+    #private;
 }
 export type MdnsDiscoveryEvents = {
-  service: (service: import("@gravitysoftware/dnssd").ServiceType) => void;
-  serviceDown: (
-    serviceDown: import("@gravitysoftware/dnssd").ServiceType
-  ) => void;
-  serviceChanged: (
-    serviceChanged: import("@gravitysoftware/dnssd").ServiceType
-  ) => void;
-  stopped: (stopped: void) => void;
-  error: (error: Error) => void;
+    service: (service: import('@gravitysoftware/dnssd').ServiceType) => void;
+    serviceDown: (serviceDown: import('@gravitysoftware/dnssd').ServiceType) => void;
+    serviceChanged: (serviceChanged: import('@gravitysoftware/dnssd').ServiceType) => void;
+    stopped: (stopped: void) => void;
+    error: (error: Error) => void;
 };
 import { TypedEmitter } from "tiny-typed-emitter";
 //# sourceMappingURL=index.d.ts.map
