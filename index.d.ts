7d3d72e194afed40a059caa7c0dab526b8629075
sethvincent <sethvincent@gmail.com>
2022-07-07 15:05:20 -0700

    format

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,33 @@
 /// <reference types="gravitysoftware__dnssd" />
 export class MdnsDiscovery extends TypedEmitter<MdnsDiscoveryEvents> {
-    constructor();
-    lookup(serviceType: dnssd.ServiceType | string): Promise<void>;
-    stopLookup(): Promise<void>;
-    announce(serviceType: dnssd.ServiceType | string, options: {
-        port: number;
-        txt: object;
-    }): void;
-    updateTxt(txt: object): void;
-    createServiceType(options: dnssd.ServiceTypeOptions): dnssd.ServiceType;
-    unannounce(immediate?: boolean | undefined): void;
-    destroy(): void;
-    #private;
+  constructor()
+  lookup(serviceType: dnssd.ServiceType | string): Promise<void>
+  stopLookup(): Promise<void>
+  announce(
+    serviceType: dnssd.ServiceType | string,
+    options: {
+      port: number
+      txt: object
+    }
+  ): void
+  updateTxt(txt: object): void
+  createServiceType(options: dnssd.ServiceTypeOptions): dnssd.ServiceType
+  unannounce(immediate?: boolean | undefined): void
+  destroy(): void
+  #private
 }
 export type MdnsDiscoveryEvents = {
-    service: (service: import('@gravitysoftware/dnssd').ServiceType) => void;
-    serviceDown: (serviceDown: import('@gravitysoftware/dnssd').ServiceType) => void;
-    serviceChanged: (serviceChanged: import('@gravitysoftware/dnssd').ServiceType) => void;
-    stopAnnouncing: (stopAnnouncing: void) => void;
-    stopLookup: (stopLookup: void) => void;
-    error: (error: Error) => void;
-};
-import { TypedEmitter } from "tiny-typed-emitter";
-import dnssd from "@gravitysoftware/dnssd";
+  service: (service: import('@gravitysoftware/dnssd').ServiceType) => void
+  serviceDown: (
+    serviceDown: import('@gravitysoftware/dnssd').ServiceType
+  ) => void
+  serviceChanged: (
+    serviceChanged: import('@gravitysoftware/dnssd').ServiceType
+  ) => void
+  stopAnnouncing: (stopAnnouncing: void) => void
+  stopLookup: (stopLookup: void) => void
+  error: (error: Error) => void
+}
+import { TypedEmitter } from 'tiny-typed-emitter'
+import dnssd from '@gravitysoftware/dnssd'
 //# sourceMappingURL=index.d.ts.map
